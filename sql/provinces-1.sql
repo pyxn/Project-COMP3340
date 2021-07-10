9392f2a11d58a5d2dc38b0da55611fee2948bfa3
Chen Qiao <65835990+ChenQiao310@users.noreply.github.com>
2021-07-10 14:09:43 +0800

    Add files via upload

---
+++ sql/provinces-1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE provinces(
+   ID                INTEGER  NOT NULL PRIMARY KEY 
+  ,province          VARCHAR(25) NOT NULL
+  ,livable_citytown1 VARCHAR(25)
+  ,livable_citytown2 VARCHAR(25)
+  ,livable_citytown3  VARCHAR(25)
+);
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (59,'British Columbia','Langford','Kelowna','Rossland');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (24,'Quebec','Trois-Rivieres','Quebec City','Saguenay (CMA), Quebec');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (62,'Nunavut',NULL,NULL,NULL);
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (11,'Prince Edward Island','Charlottetown','Summerside',NULL);
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (47,'Saskatchewan','Saskatoon','Regina','Moose Jaw');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (60,'Yukon','Whitehorse',NULL,NULL);
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (46,'Manitoba','Brandon','Winnipeg','Portage la Prairie');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (35,'Ontario','Niagara-on-the-Lake','Ottawa-Gatineau','Windsor');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (13,'New Brunswick','Bathurst','Miramichi','Quispamsis-Rothesay');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (61,'Northwest Territories','Yellowknife',NULL,NULL);
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (48,'Alberta','Calgary','Canmore','Cochrane');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (10,'Newfoundland and Labrador','Corner Brook','St. John''s','Clarenville');
+INSERT INTO provinces(ID,province,livable_citytown1,livable_citytown2,livable_citytown) VALUES (12,'Nova Scotia','Sydney','Halifax','Yarmouth');
