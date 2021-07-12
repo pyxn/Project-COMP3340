ac53f3a79376d7cb6d1b93c7c9e4f36335d6beb2
Pao Yu <51064667+pyxn@users.noreply.github.com>
2021-07-11 20:24:50 -0400

    add credential array

--- public_html/validation.php
+++ public_html/validation.php
@@ -1,13 +1,26 @@
 <?php
 
+/**
+ * ---------------------------------------------------------------------------------
+ * SQL CONNECTION CREDENTIALS
+ * ---------------------------------------------------------------------------------
+ * Configure SQL Connection using configuration file in server
+ * ---------------------------------------------------------------------------------
+ */
+$sql_configuration_array    = parse_ini_file("../sql-config.ini", true);
+$db_name                    = $sql_configuration_array['database']['database'];
+$db_hostname                = $sql_configuration_array['database']['hostname'];
+$db_username                = $sql_configuration_array['database']['username'];
+$db_password                = $sql_configuration_array['database']['password'];
+
 session_start();
 
-$con = mysqli_connect('localhost', 'USERNAME', 'PASSWORD');
-if(!$con){
-    die("connection fail: ". mysqli_connect_error());
+$con = mysqli_connect($db_hostname, $db_username, $db_password);
+if (!$con) {
+    die("connection fail: " . mysqli_connect_error());
 }
 
-mysqli_select_db($con, 'DATABASE_NAME');
+mysqli_select_db($con, $db_name);
 
 $name = $_POST['user'];
 $pass = $_POST['password'];
@@ -16,11 +29,9 @@ $s = "select * from users where username = '$name' && password = '$pass' ";
 $result = mysqli_query($con, $s);
 $num = mysqli_num_rows($result);
 
-if($num ==1){
+if ($num == 1) {
     $_SESSION['username'] = $name;
     header('location:index.php');
-}else{
-    echo"incorrect username/password";
+} else {
+    echo "incorrect username/password";
 }
-
-?>
