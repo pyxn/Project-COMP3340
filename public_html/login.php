07d9b094e983933c36c529e3ffbce9722cc5cc91
Pao Yu <51064667+pyxn@users.noreply.github.com>
2021-07-21 20:23:29 -0400

    reorganize files

--- public_html/login.php
+++ public_html/login.php
@@ -1,12 +1,14 @@
 <html>
-    <head>
-        <title>User Login</title>
-        <link rel="stylesheet" type="text/css" href="loginStyle.css">
-        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="login-box">
+
+<head>
+    <title>User Login</title>
+    <link rel="stylesheet" type="text/css" href="styles/loginStyle.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="login-box">
             <div class="row">
 
                 <div class="col-md-6 login-left">
@@ -14,11 +16,11 @@
                     <form action="validation.php" method="post">
                         <div class="form-group">
                             <label>Username</label>
-                            <input type="text" name="user" class="form-control" require> 
-                        </div> 
+                            <input type="text" name="user" class="form-control" require>
+                        </div>
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" name="password" class="form-control" require> 
+                            <input type="password" name="password" class="form-control" require>
                         </div>
                         <button type="submit" class="btn btn-primary">Login</button>
                     </form>
@@ -29,18 +31,19 @@
                     <form action="registration.php" method="post">
                         <div class="form-group">
                             <label>Username</label>
-                            <input type="text" name="user" class="form-control" require> 
-                        </div> 
+                            <input type="text" name="user" class="form-control" require>
+                        </div>
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" name="password" class="form-control" require> 
+                            <input type="password" name="password" class="form-control" require>
                         </div>
                         <button type="submit" class="btn btn-primary">Register</button>
                     </form>
-                </div> 
+                </div>
 
             </div>
-            </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
