5dadb4320340ead6cbc6aa7905ccc6e123952d40
Pao Yu <51064667+pyxn@users.noreply.github.com>
2021-07-21 22:35:40 -0400

    test database helper

--- public_html/favorite.php
+++ public_html/favorite.php
@@ -1,6 +1,30 @@
 <?php
 
+require_once('agents/DatabaseHelper.php');
+
+$sql_configuration_array    = parse_ini_file("../../../../sql-config.ini", true);
+
+// Test server config location
+if ($_SERVER['SERVER_NAME'] == 'newcitybetterlife.com' || $_SERVER['HTTP_HOST'] == 'newcitybetterlife.com') {
+    $sql_configuration_array    = parse_ini_file("../sql-config.ini", true);
+}
+
+$db_name                    = $sql_configuration_array['database']['database'];
+$db_hostname                = $sql_configuration_array['database']['hostname'];
+$db_username                = $sql_configuration_array['database']['username'];
+$db_password                = $sql_configuration_array['database']['password'];
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['toggle-city-favorite'])) {
+
+    // Check if we have the right city rank selected
     $selected_city_rank = $_POST['toggle-city-favorite'];
     echo "Working with SELECTED_CITY_RANK: " . $selected_city_rank;
+
+    // Use the ULTIMATE PAO shortcut for making database changes
+    $database_helper = new DatabaseHelper($db_hostname, $db_name, $db_username, $db_password);
+    $selected_city_record_associative_array = $database_helper->get("SELECT * FROM cities WHERE city_rank = $selected_city_rank");
+    echo "<pre>";
+    echo print_r($selected_city_record_associative_array);
+    echo "</pre>";
 }
---
+++ public_html/helpers/DatabaseHelper.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * CLASS: DatabaseHelper
+ * a helper object class that provides a shortcut to getting
+ * arrays from a database and also updating its values.
+ */
+
+class DatabaseHelper {
+
+    private $db_hostname;
+    private $db_database;
+    private $db_username;
+    private $db_password;
+
+    public function __construct($hostname, $database, $username, $password) {
+        $this->db_hostname = $hostname;
+        $this->db_database = $database;
+        $this->db_username = $username;
+        $this->db_password = $password;
+    }
+    /**
+     * Queries the database using an SQL query parameter and
+     * returns the result as an associative array.
+     */
+    public function get($sql_query) {
+        // Attempt database connection and then execute the query
+        try {
+            $data_source_hostname = $this->db_hostname;
+            $data_source_database = $this->db_database;
+            $data_source_username = $this->db_username;
+            $data_source_password = $this->db_password;
+            $sql_connection = new PDO("mysql:host=$data_source_hostname;dbname=$data_source_database", $data_source_username, $data_source_password);
+            $sql_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql_statement = $sql_connection->prepare($sql_query);
+            $sql_statement->execute();
+            $sql_result_array = $sql_statement->fetchAll(\PDO::FETCH_ASSOC);
+            // Return an error message on exception
+        } catch (PDOException $exception) {
+            echo "Exception: " . $exception->getMessage();
+            $sql_connection = null;
+        }
+        $sql_connection = null;
+        return $sql_result_array;
+    }
+
+    /**
+     * Queries the database but does not return anything.
+     * This function is used to make changes to the database.
+     */
+    public function set($sql_query) {
+
+        try {
+            $data_source_hostname = $this->db_hostname;
+            $data_source_database = $this->db_database;
+            $data_source_username = $this->db_username;
+            $data_source_password = $this->db_password;
+            $sql_connection = new PDO("mysql:host=$data_source_hostname;dbname=$data_source_database", $data_source_username, $data_source_password);
+            $sql_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql_connection->exec($sql_query);
+        } catch (PDOException $exception) {
+            echo $sql_query . "<br>" . $exception->getMessage();
+        }
+
+        $sql_connection = null;
+    }
+}
