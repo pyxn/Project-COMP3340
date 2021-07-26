2be0f64ba9ce7e456a6c936d6eb4c09523c39ceb
xuyijiu19 <67746130+xuyijiu19@users.noreply.github.com>
2021-07-26 17:38:59 +0800

    Add files via upload

--- public_html/profile.php
+++ public_html/profile.php
@@ -1,1 +1,31 @@
-<?php echo "the user profile page goes here";
+<?php
+//  include_once 'includes/dbh.inc.php';
+//?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>User Profile</title>
+  </head>
+  <body>
+
+  <?php
+    $conn = mysqli_connect('localhost', 'qiao6', 'Woshishen@2021');
+    if(!$conn){
+      die("connection fail: ". mysqli_connect_error());
+    }    
+    //$selected_username      = $_POST['username'];
+    $sql = "SELECT * FROM favorites";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if($resultCheck > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['favorite_city_rank'].$row['favorite_city_name']"<br>" ;
+      }
+    }
+  ?>
+
+  </body>
+</html>
