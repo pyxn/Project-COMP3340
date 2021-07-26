19c5fee0ad238548212b36192247bf87eb5e6775
xuyijiu19 <67746130+xuyijiu19@users.noreply.github.com>
2021-07-26 17:51:49 +0800

    Update profile.php

--- public_html/profile.php
+++ public_html/profile.php
@@ -9,23 +9,9 @@
     <title>User Profile</title>
   </head>
   <body>
+    
+    <h1>My Favorites</h1>
 
-  <?php
-    $conn = mysqli_connect('localhost', 'qiao6', 'Woshishen@2021');
-    if(!$conn){
-      die("connection fail: ". mysqli_connect_error());
-    }    
-    //$selected_username      = $_POST['username'];
-    $sql = "SELECT * FROM favorites";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
-
-    if($resultCheck > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-        echo $row['favorite_city_rank'].$row['favorite_city_name']"<br>" ;
-      }
-    }
-  ?>
 
   </body>
 </html>
