a81288097ff5f19a30c8a3f6582072b5a0cc3159
Pao Yu <51064667+pyxn@users.noreply.github.com>
2021-07-27 14:32:56 -0400

    add documentation link

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -21,7 +21,7 @@ session_start();
         } 
         ?>
         <li><a href="status.php">Server Status</a></li>
-        <li><a href="documentation.html">Get Help</a></li>
+        <li><a href="https://project-comp3340.gitbook.io/project/">Documentation</a></li>
         <li><a href="#">About</a></li>
         <?php
         if (isset($_SESSION['username'])) {
