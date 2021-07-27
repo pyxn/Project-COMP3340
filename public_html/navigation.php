2455dbb94a4ed21b5f00c682cb1c06dd031fe591
Chen Qiao <65835990+ChenQiao310@users.noreply.github.com>
2021-07-27 13:50:52 +0800

    Update navigation.php

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -9,15 +9,16 @@ session_start();
     </span>
     <ul>
         <li><a href="index.php">Home Page</a></li>
+        <li><a href="dreamcity.php">Find Dream City</a></li>
         <?php
         if (isset($_SESSION['username'])) {
-            echo "<li><a href='logout.php'>Log Out</a></li>";
             echo "<li><a href='profile.php'>My Favorites</a></li>";
-        } else {
+            echo "<li><a href='logout.php'>Log Out</a></li>";
+        } 
+        else {
             echo "<li><a href='login.php'>Log In</a></li>";
         }
         ?>
-        <li><a href="dreamcity.php">Find Dream City</a></li>
         <li><a href="status.php">Server Status</a></li>
         <li><a href="documentation.html">Get Help</a></li>
         <li><a href="#">About</a></li>
