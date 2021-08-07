0ab3a404f7bda03c85cfdff25e804299e72a0931
Pao Yu <yupao@Paos-MacBook.local>
2021-08-07 00:25:11 -0400

    increase theme switcher button font size

--- public_html/helpers/ThemeHelper.php
+++ public_html/helpers/ThemeHelper.php
@@ -34,7 +34,7 @@ class ThemeHelper {
         $html_code = "
         <form method='POST' action='switchtheme.php'>
         <input type='hidden' value='$next_color' name='next-theme'>
-        <input style='position: fixed; bottom: 1.618rem; right: 1.618rem; $background_color color: white !important; padding: 1rem; border-radius: 0.618rem; color: white; z-index: 9999; border: 1px solid white; font-weight: bold;' value='Switch Theme ⟳' type='submit'>
+        <input style='position: fixed; bottom: 1.618rem; right: 1.618rem; $background_color color: white !important; padding: 1rem; font-size: 1rem; border-radius: 0.618rem; color: white; z-index: 9999; border: 1px solid white; font-weight: bold;' value='Switch Theme ⟳' type='submit'>
         </form>
         ";
         return $html_code;
