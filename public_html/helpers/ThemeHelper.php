71cb18896e4b00dbb91b0c80a6c6b0d24ecc893c
Pao Yu <yupao@Paos-MacBook.local>
2021-08-06 23:15:22 -0400

    update paths

--- public_html/helpers/ThemeHelper.php
+++ public_html/helpers/ThemeHelper.php
@@ -3,11 +3,11 @@
 class ThemeHelper {
     public static function get_css_theme(String $theme_color) {
         if ($theme_color == 'blue') {
-            return '<link rel="stylesheet" href="styles/theme_blue.css" />';
+            return '<link rel="stylesheet" href="styles/theme-blue.css" />';
         } else if ($theme_color == 'pink') {
-            return '<link rel="stylesheet" href="styles/theme_pink.css" />';
+            return '<link rel="stylesheet" href="styles/theme-pink.css" />';
         } else if ($theme_color == 'yellow') {
-            return '<link rel="stylesheet" href="styles/theme_yellow.css" />';
+            return '<link rel="stylesheet" href="styles/theme-yellow.css" />';
         } else {
             return '';
         }
