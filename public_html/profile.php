48ef092ab2c36a43120d6072f94c24648ab68165
Pao Yu <yupao@Paos-MacBook.local>
2021-08-06 23:35:52 -0400

    add theme control class to nav elements

--- public_html/profile.php
+++ public_html/profile.php
@@ -1,37 +1,39 @@
 <?php
 
 session_start();
-if(!isset($_SESSION['username'])){
+if (!isset($_SESSION['username'])) {
     header('location:login.php');
 }
 
 ?>
 <html>
+
 <head>
     <title>My Favorite Cities</title>
     <link rel="stylesheet" type="text/css" href="styles/bootstrap.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <h3 style="color: #fff;">My Favorite Cities
-	<a class="btn btn-primary" href="index.php" role="button" style="float: right; color: black; background-color: white;">Home</a></h3>
-    </div>
+    <nav class="navbar navbar-inverse theme-control">
+        <div class="container-fluid">
+            <h3 style="color: #fff;">My Favorite Cities
+                <a class="btn btn-primary" href="index.php" role="button" style="float: right; color: black; background-color: white;">Home</a>
+            </h3>
+        </div>
     </nav>
 
     <div class="container">
         <br><br><br>
         <h3 style="color: #707070;font-weight: bold;">
-        Hello, <?php echo $_SESSION['username']?></h3>
+            Hello, <?php echo $_SESSION['username'] ?></h3>
         <h4 style="color: #707070;">Here is the list of cities you like!</h4>
-        
-		<br><br><br>
+
+        <br><br><br>
         <hr>
 
         <div class="row">
-             <table class="table table-striped table-hover">
-                 <thead>
+            <table class="table table-striped table-hover">
+                <thead>
                     <tr>
                         <th>Rank</th>
                         <th>City/Town</th>
@@ -45,21 +47,21 @@ if(!isset($_SESSION['username'])){
                 <tbody>
                     <?php
                     $conn = mysqli_connect('localhost', 'qiao6', 'Woshishen@2021');
-                    if(!$conn){
-                        die("connection fail: ". mysqli_connect_error());
+                    if (!$conn) {
+                        die("connection fail: " . mysqli_connect_error());
                     }
 
                     mysqli_select_db($conn, 'qiao6_comp3340');
-                    $username=$_SESSION['username'];
+                    $username = $_SESSION['username'];
                     $query = "SELECT * from favorites WHERE username='$username'";
                     $data = mysqli_query($conn, $query);
-                    if(mysqli_num_rows($data)>0){
-                        while($row = mysqli_fetch_assoc($data)){
-                            $rank=$row["favorite_city_rank"];
+                    if (mysqli_num_rows($data) > 0) {
+                        while ($row = mysqli_fetch_assoc($data)) {
+                            $rank = $row["favorite_city_rank"];
                             $query = "SELECT * from cities WHERE rank='$rank'";
                             $city = mysqli_query($conn, $query);
                             $city_row = mysqli_fetch_assoc($city);
-                            
+
                             $cityname = $city_row["city_town"];
                             $province = $city_row["province"];
                             $pop = $city_row["population"];
@@ -67,36 +69,38 @@ if(!isset($_SESSION['username'])){
                             $min_income = $city_row["min_income_required_20_down"];
                             $rating = $city_row["scenery_rating"];
                             echo "<tr onclick=\"window.location='city.php?rk=$rank'\" >";
-                            ?>
+                    ?>
                             <td><?php echo $rank; ?></td>
-                                <td><?php echo $cityname; ?></td>
-                                <td><?php echo $province; ?></td>
-                                <td><?php echo $pop; ?></td>
-                                <td>$<?php echo $avg_price; ?></td>
-                                <td>$<?php echo $min_income; ?></td>
-                                <td><?php for($i=0; $i<$rating; $i++){echo "☆";} ?></td>
+                            <td><?php echo $cityname; ?></td>
+                            <td><?php echo $province; ?></td>
+                            <td><?php echo $pop; ?></td>
+                            <td>$<?php echo $avg_price; ?></td>
+                            <td>$<?php echo $min_income; ?></td>
+                            <td><?php for ($i = 0; $i < $rating; $i++) {
+                                    echo "☆";
+                                } ?></td>
                             </tr>
-                            <?php
+                        <?php
                         }
-                    }
-                    else{
+                    } else {
                         echo "<tr onclick=\"window.location='dreamcity.php?rk=$rank'\" >";
                         ?>
-                        
-                            <td colspan="7">You haven't added any city yet. Click here to find your dream city!</td>
+
+                        <td colspan="7">You haven't added any city yet. Click here to find your dream city!</td>
                         </tr>
-                        <?php
+                    <?php
                     }
                     ?>
                 </tbody>
-             </table>
+            </table>
         </div>
         <hr>
 
         <h5 style="color: #707070;">If you want to change your username or password</h5>
         <form class="form-horizontal" action="userchange.php" method="POST">
-        <input type="submit" class="btn btn-primary" name="submit" value="Click Here">
+            <input type="submit" class="btn btn-primary" name="submit" value="Click Here">
         </form>
 
     </div>
-<body>
+
+    <body>
--- public_html/userchange.php
+++ public_html/userchange.php
@@ -1,13 +1,14 @@
 <?php
 
 session_start();
-if(!isset($_SESSION['username'])){
+if (!isset($_SESSION['username'])) {
     header('location:login.php');
 }
 
 ?>
 
 <html>
+
 <head>
     <title>Change Username|Password</title>
     <link rel="stylesheet" type="text/css" href="styles/bootstrap.css">
@@ -16,51 +17,48 @@ if(!isset($_SESSION['username'])){
 <body>
     <?php
     $conn = mysqli_connect('localhost', 'qiao6', 'Woshishen@2021');
-    if(!$conn){
-        die("connection fail: ". mysqli_connect_error());
+    if (!$conn) {
+        die("connection fail: " . mysqli_connect_error());
     }
 
     mysqli_select_db($conn, 'qiao6_comp3340');
-    $name=$_SESSION['username'];
+    $name = $_SESSION['username'];
 
     $query = "SELECT * from users WHERE username='$name'";
     $data = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($data);
-    $pass=$row["password"];
+    $pass = $row["password"];
 
-    if(isset($_POST["edit"])){
-        $inputName=$_POST["name"];
-        $inputPass=$_POST["pass"];
+    if (isset($_POST["edit"])) {
+        $inputName = $_POST["name"];
+        $inputPass = $_POST["pass"];
 
         $s = "select * from users where username='$inputName' ";
         $result = $conn->query($s);
-        
-        if($result->num_rows>0 && $inputName!=$_SESSION['username']){
+
+        if ($result->num_rows > 0 && $inputName != $_SESSION['username']) {
             echo '<script>alert("This username is not available.")</script>';
-        }
-        else if($inputName==""){
+        } else if ($inputName == "") {
             echo '<script>alert("Please fill in username.")</script>';
-        }
-        else if($inputPass==""){
+        } else if ($inputPass == "") {
             echo '<script>alert("Please fill in password.")</script>';
-        }
-        else{
-            $prName=$_SESSION['username'];
-            $r="UPDATE users SET username = '$inputName', password = '$inputPass' WHERE username='$prName' ";
+        } else {
+            $prName = $_SESSION['username'];
+            $r = "UPDATE users SET username = '$inputName', password = '$inputPass' WHERE username='$prName' ";
             mysqli_query($conn, $r);
-	    $r="UPDATE favorites SET username = '$inputName' WHERE username='$prName' ";
+            $r = "UPDATE favorites SET username = '$inputName' WHERE username='$prName' ";
             mysqli_query($conn, $r);
             header('location:login.php');
         }
     }
-?>
-    <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <h3 style="color: #fff;">Change Username|Password
-	<a class="btn btn-primary" href="index.php" role="button" style="float: right; color: black; background-color: white;">Home</a>
-        <a class="btn btn-primary" href="profile.php" role="button" style="float: right; color: black; background-color: white;">Back</a>
-	</h3>
-    </div>
+    ?>
+    <nav class="navbar navbar-inverse theme-control">
+        <div class="container-fluid">
+            <h3 style="color: #fff;">Change Username|Password
+                <a class="btn btn-primary" href="index.php" role="button" style="float: right; color: black; background-color: white;">Home</a>
+                <a class="btn btn-primary" href="profile.php" role="button" style="float: right; color: black; background-color: white;">Back</a>
+            </h3>
+        </div>
     </nav>
 
     <div class="container">
@@ -68,25 +66,25 @@ if(!isset($_SESSION['username'])){
         <div class="row">
             <form class="form-horizontal" action="userchange.php" method="POST">
                 <div class="form-group">
-				<label class="col-lg-2 control-label">Username</label>
-				<div class="col-lg-4">
-				    <input type="text" name="name" class="form-control" placeholder=<?php echo $name; ?>>
-				</div>
-			    </div>
+                    <label class="col-lg-2 control-label">Username</label>
+                    <div class="col-lg-4">
+                        <input type="text" name="name" class="form-control" placeholder=<?php echo $name; ?>>
+                    </div>
+                </div>
 
                 <div class="form-group">
-				<label class="col-lg-2 control-label">Password</label>
-				<div class="col-lg-4">
-				    <input type="text" name="pass" class="form-control" placeholder=<?php echo $pass; ?>>
-				</div>
-			    </div>
+                    <label class="col-lg-2 control-label">Password</label>
+                    <div class="col-lg-4">
+                        <input type="text" name="pass" class="form-control" placeholder=<?php echo $pass; ?>>
+                    </div>
+                </div>
 
                 <div class="form-group">
-				<label class="col-lg-2 control-label"></label>
-				<div class="col-lg-4">
-				    <input type="submit" class="btn btn-primary" name="edit">
-				</div>
-			    </div>
+                    <label class="col-lg-2 control-label"></label>
+                    <div class="col-lg-4">
+                        <input type="submit" class="btn btn-primary" name="edit">
+                    </div>
+                </div>
             </form>
         </div>
     </div>
