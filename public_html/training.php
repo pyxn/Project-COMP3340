44586b65a7494fceb03d353a7b50b614214167e7
xuyijiu19 <67746130+xuyijiu19@users.noreply.github.com>
2021-08-11 03:00:05 +0800

    Add files via upload

--- public_html/training.php
+++ public_html/training.php
@@ -46,7 +46,7 @@ session_start();
     <style>
 	.video-container { position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; overflow: hidden; }
 
-	.video-container iframe, .video-container object, .video-container embed { position: absolute; top: 0; left: 0; width: 90%; height: 90%; }
+	.video-container iframe, .video-container object, .video-container embed { position: absolute; top: 0; left: 5%; width: 90%; height: 90%; }
 
     </style>
 
